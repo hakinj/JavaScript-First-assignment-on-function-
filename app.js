f9c10946ca9da2f5b7544fb9c666b7df5b3408bd
Akinola Akinjide <akinjide19@gmail.com>
2023-09-18 21:27:01 -0500

    add files

---
+++ app.js
@@ -0,0 +1,12 @@
+function bio(name, weight){
+    name = prompt("enter name");
+    weight = Number(prompt("enter weight in kg to covert to pounds"));
+    pounds = weight/2.2046;
+    return `My name is ${name} and i weight ${pounds + " pounds"}`;
+    alert
+
+
+
+}
+alert(bio())
+console.log(bio());
